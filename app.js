72058d37689d018fc479a8f348d95401682a0324
unenglishable <unenglishable@gmail.com>
2016-01-08 23:38:59 -1000

    move public to assets

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var express = require('express');
 var app = express();
 
 app.set('port', process.env.PORT || 8080);
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/assets')));
 app.get('*', function (req, res) {
-  res.sendFile('index.html', {root: __dirname + '/public'});
+  res.sendFile('index.html', {root: __dirname + '/assets'});
 });
 
 app.listen(app.get('port'));
