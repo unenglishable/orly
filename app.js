6df5edceb0da1ea8284d346885ff35b0e2de6e8e
unenglishable <unenglishable@gmail.com>
2016-01-08 22:24:29 -1000

    initialize appjs

---
+++ app.js
@@ -0,0 +1,10 @@
+require('dotenv').load();
+var path = require('path');
+var express = require('express');
+var app = express();
+
+app.set('port', process.env.PORT || 8080);
+app.use(express.static(path.join(__dirname, '/public')));
+app.get('*', function (req, res) {
+  res.sendFile('index.html', {root: __dirname + '/public'});
+});
