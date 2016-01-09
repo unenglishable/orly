e0a8a231baa186bd95fa2a8544cae059d0eb857c
unenglishable <unenglishable@gmail.com>
2016-01-08 23:51:26 -1000

    using browserify; compiling to dist
    point static served files to dist/ for express

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 
 app.set('port', process.env.PORT || 8080);
-app.use(express.static(path.join(__dirname, '/assets')));
+app.use(express.static(path.join(__dirname, '/dist')));
 app.get('*', function (req, res) {
   res.sendFile('index.html', {root: __dirname + '/assets'});
 });
