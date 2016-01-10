301f0376cb568a0ea93a094338918723d94b3fab
unenglishable <unenglishable@gmail.com>
2016-01-09 16:28:59 -1000

    add simple message to display in result section

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,22 @@
 var m = require('mithril');
 
+var message = m.prop('');
+
+var result = {
+  view: function() {
+    var input = message();
+    var output = '';
+    if (input !== '') {
+      output = 'Hit enter to check if "' + input + '" is a thing!';
+      console.log('message: "'+input+'"');
+    }
+    else {
+      output = 'Is it a thing?!';
+    }
+    return m('div', output);
+  }
+};
+
 var isItASearchBox = {
   view: function() {
     return m('input');
@@ -7,3 +24,4 @@ var isItASearchBox = {
 };
 
 m.module(document.getElementById('isItASearchBox'), isItASearchBox);
+m.module(document.getElementById('result'), result);
