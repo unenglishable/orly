496ca665c936478c9c0f60aed411894e2ec5260c
unenglishable <unenglishable@gmail.com>
2016-02-21 17:57:20 -1000

    move stuff around

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 require('dotenv').config({silent: true});
 var path = require('path');
 var express = require('express');
+var browserify = require('browserify-middleware');
 var app = express();
+var clientDir = path.join(__dirname, '..', 'client');
 
 app.set('port', process.env.PORT || 8080);
-app.use(express.static(path.join(__dirname, '/dist')));
+app.use(express.static(clientDir));
+app.use('/api', api);
+app.get('/app.js', browserify(path.join(clientDir, 'js', 'app.js')), {});
 app.get('*', function (req, res) {
   res.sendFile('index.html', {root: __dirname + '/assets'});
 });
