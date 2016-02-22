7078f3c078f73f5e61cff87e74907accfd230d74
unenglishable <unenglishable@gmail.com>
2016-02-22 11:15:02 -1000

    remove server/db/

--- server/db/db.js
+++
@@ -1,5 +0,0 @@
-var mongoose = require('mongoose');
-var mongooseBird = require('mongoose-bird')(mongoose);
-var mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/myproject';
-mongoose.connect(mongoUrl);
-module.exports = mongoose;
--- server/db/index.js
+++
@@ -1,5 +0,0 @@
-var path = require('path');
-var db = module.exports = {
-  database: require(path.join(__dirname, 'db')),
-  things: require(path.join(__dirname, 'things'))
-};
